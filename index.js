280bf44e4f8f091c51558bfffb067c8e4f737b06
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-08-22 08:43:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun("swim")
+const mondayWork = function (activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+mondayWork("work from home")
+
+function wrapAdjective(param = "*"){
+
+    return function(param1 = "special"){
+        return `You are ${param}${param1}${param}!`
+    }
+}
+console.log (wrapAdjective()("hard worker"));
